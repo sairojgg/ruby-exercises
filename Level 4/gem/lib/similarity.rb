2a4ae466a489b5eaa6578d985ca5a78687406d6e
Sairo Guanipa <sairojgg@gmail.com>
2021-03-08 20:14:06 +0000

    Level 4 first two exercises done

---
+++ Level 4/gem/lib/similarity.rb
@@ -0,0 +1,34 @@
+class Similarity
+    def self.calculate_distance(source, comparison)
+        raise "This method cannot compare to NilClass" if comparison.nil?
+        n = source.length
+        m = comparison.length
+        max = n/2
+
+        # Returns either word's length if one of them is empty
+        return m if 0 == n
+        return n if 0 == m
+
+        # Checks if both words are equal and returns 0
+        return 0 if source.eql? comparison
+
+        d = (0..m).to_a
+        x = nil
+        source.each_char.with_index do |char1, i|
+            e = i+1
+            comparison.each_char.with_index do |char2, j|
+                cost = (char1 == char2) ? 0 : 1
+                x = 
+                [
+                    d[j+1] + 1, # insertion
+                    e + 1,      # deletion
+                    d[j] + cost # substitution
+                ].min
+                d[j] = e
+                e = x
+            end
+            d[m] = x
+        end
+        return x
+    end
+end
---
+++ Level 4/gem/similarity.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'SimilaritySairo'
+  s.version     = '0.0.1'
+  s.summary     = "Compares two strings and returns the amount of modifications between the two"
+  s.description = "A similarity gem"
+  s.authors     = ["Sairo Guanipa"]
+  s.email       = 'sairo.guanipa@bairesdev.com'
+  s.files       = ["lib/similarity.rb"]
+  s.homepage    =
+    'https://rubygems.org/gems/similarity'
+  s.license       = 'MIT'
+end
